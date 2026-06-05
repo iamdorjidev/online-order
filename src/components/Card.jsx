8f22edd4a58bd437eb895e41d95832f8762bc0bf
Thinley Dorji <thinleysangay@Thinleys-MacBook-Air.local>
2026-06-05 21:53:15 +1200

    header changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,22 +3,22 @@ import Button from './button/Button'
 const Card = (props) => {
   return (
     <div>
-        <main class="main bd-grid">
-            <article class="card">
-                <div class="card__img">
+        <main className="main bd-grid">
+            <article className="card">
+                <div className="card__img">
                     <img src={props.image} alt="" />
                 </div>
-                <div class="card__name">
+                <div className="card__name">
                     <p>{props.name}</p>
                 </div>
-                <div class="card__precis">
-                    <a href="" class="card__icon" ><ion-icon name="heart-outline"></ion-icon></a>
+                <div className="card__precis">
+                    <a href="" className="card__icon" ><ion-icon name="heart-outline"></ion-icon></a>
                     
                     <div>
-                        <span class="card__preci card__preci--before">${props.beforeprice}</span>
-                        <span class="card__preci card__preci--now">${props.afterprice}</span>
+                        <span className="card__preci card__preci--before">${props.beforeprice}</span>
+                        <span className="card__preci card__preci--now">${props.afterprice}</span>
                     </div>
-                    <a href="" class="card__icon"><ion-icon name="cart-outline"></ion-icon></a>
+                    <a href="" className="card__icon"><ion-icon name="cart-outline"></ion-icon></a>
                     <Button />
                 </div>
             </article>
--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import style from './Button.module.css'
 const Button = () => {
+  function btnClick(){
+    console.log('clicked btn')
+  }
   return (
     <div>
-      <button type="submit" className={style.button}>Add</button>
+      
+      <button type="submit" className={style.button} onClick={btnClick}>
+        Add to cart
+      </button>
     </div>
   )
 }
